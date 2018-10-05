b17aec08fd5e8903e30cf05fa8424f8b6939771f
davidnicholastaylor <dntaylor14@gmail.com>
2018-10-05 12:00:39 -0500

    initialized repository created dictionary added key value pairs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dictionaries {
+    class Program {
+        static void Main (string[] args) {
+
+            // Create dictionary named stocks containing strings
+            Dictionary<string, string> stocks = new Dictionary<string, string> ();
+
+            // Add key valur pairs to stocks dictionary
+            stocks.Add ("CGC", "Canopy Growth");
+            stocks.Add ("RDWR", "Radware");
+            stocks.Add ("CY", "Cypress Semiconductor");
+            stocks.Add ("CRON", "Cronos Group");
+            stocks.Add ("SSTI", "ShotSpotter");
+
+        }
+    }
+}
